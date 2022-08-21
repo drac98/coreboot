cbcfab8271561fb4210e5417ce39fa459975522d
Husni Faiz <ahamedhusni73@gmail.com>
2022-08-21 18:14:19 +0530

    console: ramstage smbus console support
    Change-Id: Ib76a7b146cd2995fc12b8202cf12e0fcd33be5ba

--- src/drivers/uart/i2c_smbus_console.c
+++ src/drivers/uart/i2c_smbus_console.c
@@ -7,10 +7,19 @@
 
 void i2c_smbus_console_init(void)
 {
+#if (CONFIG(CONSOLE_SERIAL) && ENV_ROMSTAGE)
 	early_pch_init();
+#endif
 }
 
 void i2c_smbus_console_tx_byte(unsigned char c)
 {
+#if (CONFIG(CONSOLE_SERIAL) && ENV_ROMSTAGE)
 	smbus_write_byte(0x48, 0x0, c);
+#endif
+
+#if (CONFIG(CONSOLE_SERIAL) && ENV_RAMSTAGE)
+	do_smbus_write_byte(0x400, 0x48, 0x0, c);
+#endif
+
 }
